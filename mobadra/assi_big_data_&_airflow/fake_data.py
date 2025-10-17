dc03f072173eab66e4c717a936705bf791aabe84
KirellosNader <kirellos.farag.20@gmail.com>
2025-10-17 21:55:06 +0300

    Refactor Kafka producer configuration and comments

--- mobadra/assi_big_data_&_airflow/fake_data.py
+++ mobadra/assi_big_data_&_airflow/fake_data.py
@@ -3,17 +3,16 @@ import json
 from faker import Faker
 from kafka import KafkaProducer
 
-# إعداد Faker
+
 fake = Faker()
 
-# تأكد من استخدام المنفذ الخارجي الصحيح: 29092
-# ملاحظة: يجب أن يكون هذا 29092 بدلاً من 9092
+
 KAFKA_BROKER = 'localhost:29092' 
 KAFKA_TOPIC = 'gps_topic'
 
 producer = KafkaProducer(
     bootstrap_servers=[KAFKA_BROKER],
-    # طريقة تحويل البيانات إلى بايت قبل الإرسال
+   
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
@@ -34,12 +33,12 @@ try:
     while True:
         gps_data = generate_gps_data()
         
-        # إرسال البيانات إلى Kafka
+       
         producer.send(KAFKA_TOPIC, value=gps_data)
         
         print(f"Sent: {gps_data}")
         
-        # انتظار لمدة ثانية قبل إرسال البيانات التالية
+       
         time.sleep(1) 
 
 except KeyboardInterrupt:
